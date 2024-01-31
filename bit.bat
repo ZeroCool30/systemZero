61779ae06adebc3f5890ce4f0a25d0e76c47bd8e
Gabino <gabinosalinas.19@gmail.com>
2024-01-31 14:47:03 -0600

    Add files

---
+++ bit.bat
@@ -0,0 +1,27 @@
+@echo off
+title Git
+echo.
+echo.
+echo.
+git status
+echo.
+echo.
+echo.
+echo.
+pause>nul
+echo.
+echo.
+echo.
+git add .
+echo.
+echo.
+git commit -m "Add files"
+echo.
+echo.
+echo.
+pause>nul
+git push -u origin main
+echo.
+echo.
+echo.
+pause>nul
